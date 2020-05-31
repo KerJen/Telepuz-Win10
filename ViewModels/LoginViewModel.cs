abd4347a97e22badf098cfe68b3479cff4e27bd7
KerJen <kerjen01@gmail.com>
2020-05-31 22:56:11 +0300

    [FEATURE] Added ale Sound and slogan animation

--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -6,8 +6,12 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+using Windows.Storage;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
+using Telepuz.Helpers;
 using Telepuz.Models.Business;
 using Telepuz.Models.Business.Model.Nickname;
 using Telepuz.Models.Network;
@@ -20,11 +24,20 @@ namespace Telepuz.ViewModels
     {
         readonly Regex nicknameRegex = new Regex("^[A-zА-яЁё0-9\\s]{1,30}$");
 
+        readonly Random rand = new Random();
+
+        readonly MediaPlayer player = new MediaPlayer();
+
         readonly TelepuzWebSocketService _client;
 
         public LoginViewModel()
         {
+            SloganClick = new RelayCommand(PlayAle);
             EnterButtonClick = new RelayCommand(SendNickname, NicknameCheck);
+
+            player.Source = MediaSource.CreateFromUri(new Uri("ms-appx:///Assets/ale.mp3"));
+
+            SetPhrases();
 
             _client = TelepuzWebSocketService.Client;
 
@@ -32,6 +45,35 @@ namespace Telepuz.ViewModels
         }
 
         public RelayCommand EnterButtonClick { get; private set; }
+        public RelayCommand SloganClick { get; private set; }
+
+        string _phrase;
+        public string Phrase
+        {
+            get => _phrase;
+            set
+            {
+                _phrase = value;
+                RaisePropertyChanged("Phrase");
+            }
+        }
+
+        async void SetPhrases()
+        {
+            while (true)
+            {
+                foreach (var phrase in StringHelper.aleStrings)
+                {
+                    Phrase = phrase;
+                    await Task.Delay(rand.Next(1000,1300));
+                }
+            }
+        }
+
+        void PlayAle()
+        {
+            player.Play();
+        }
 
         // TODO: Посмотреть способ сокращения записи
         string _nickname;
@@ -49,14 +91,14 @@ namespace Telepuz.ViewModels
         bool NicknameCheck()
         {
             return Nickname != null && nicknameRegex.IsMatch(Nickname);
-        } 
+        }
 
         // TODO: Перенести в репозитории
         void SendNickname()
         {
             _client.Once("auth.login", (response) =>
             {
-                if (response.Result == (int) Results.OK)
+                if (response.Result == (int)Results.OK)
                 {
 
                 }
